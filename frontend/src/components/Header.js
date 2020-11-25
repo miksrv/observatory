3d00672143177539aa2c3b5c6d7929379ff7518e
Mikhail Topchilo <miksoft.tm@gmail.com>
2020-11-25 14:29:10 +0500

    Add header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Checkbox, Container, Icon } from 'semantic-ui-react'
+
+import moment from 'moment'
 
 class Header extends Component {
 
-    render() {
-        return (
-            <Container>
+    state = {
 
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+    render() {
+        const { onClickMenu } = this.props
+
+        return (
+            <Container className='header-toolbar'>
+                <Icon
+                    className='hamburger'
+                    name='bars'
+                    size='big'
+                    onClick={() => onClickMenu()}
+                />
+                <span className='last-update'>
+
+                </span>
+                <Checkbox
+                    toggle
+                    // checked={autoUpdate}
+                    label=''
+                    // onChange={this.handleChangeAutoupdate}
+                    className='update-switch'
+                />
             </Container>
         )
     }

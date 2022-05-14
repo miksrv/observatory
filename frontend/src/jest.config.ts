49949f6f3b940e016790c00179f5ee3a8923b092
Mik <miksoft.tm@gmail.com>
2022-05-14 10:26:46 +0500

    Update GitHub actions, SonarCloud integration

---
+++ frontend/src/jest.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+    verbose: true,
+    testEnvironment: 'jsdom',
+    collectCoverageFrom: [
+        '**/src/**/*.{js,jsx,ts,tsx}',
+        '!**/src/*.{js,jsx,ts,tsx}',
+    ],
+    coveragePathIgnorePatterns: [
+        './src/*/*.types.{ts,tsx}',
+        './src/index.tsx',
+        './src/App.tsx',
+        './src/serviceWorker.ts'
+    ],
+    reporters: [
+        'lcov',
+    ],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/ts-jest'
+    },
+    transformIgnorePatterns: [
+        '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+        '^.+\\.module\\.(css|sass|scss)$'
+    ],
+    moduleNameMapper: {
+        '^react-native$': 'react-native-web',
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+        'src/(.*)$': '<rootDir>/src/$1'
+    }
+}
+
+export default config
